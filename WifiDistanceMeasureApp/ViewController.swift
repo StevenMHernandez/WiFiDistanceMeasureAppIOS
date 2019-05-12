4cc1cb2d4a4c968bafb5c974bb58d792f1ce1a7f
StevenMHernandez <contact@stevenhdesigns.com>
2019-05-12 09:13:38 -0400

    Initial Commit

---
+++ WifiDistanceMeasureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WifiDistanceMeasureApp
+//
+//  Created by Steven Hernandez on 5/12/19.
+//  Copyright © 2019 MoWiNG Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
