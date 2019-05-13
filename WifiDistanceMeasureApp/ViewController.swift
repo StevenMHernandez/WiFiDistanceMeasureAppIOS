82b0bea5f41b77536d35dc7ec7fff9942bf9dc34
StevenMHernandez <contact@stevenhdesigns.com>
2019-05-13 13:37:53 -0400

    Make line chart view updatable without requiring additional boilerplate in the controller

--- WifiDistanceMeasureApp/ViewController.swift
+++ WifiDistanceMeasureApp/ViewController.swift
@@ -11,88 +11,44 @@ import CocoaAsyncSocket
 import Charts
 
 
-// @See https://stackoverflow.com/a/49571718
-// This seems to have been removed from swift at some point
-extension Date {
-
-    func millisecondsSince1970() -> Int64! {
-        return Int64(self.timeIntervalSince1970 * 1000)
-    }
-    
-    init(millis: Int64) {
-        self = Date(timeIntervalSince1970: TimeInterval(millis / 1000))
-        self.addTimeInterval(TimeInterval(Double(millis % 1000) / 1000 ))
-    }
-    
-}
-
 class ViewController: UIViewController, StreamDelegate, GCDAsyncUdpSocketDelegate, ChartViewDelegate {
     
-    @IBOutlet weak var actualLineChart: LineChartView!
+    @IBOutlet weak var actualLineChart: UpdatableLineChartView!
     var socket: GCDAsyncUdpSocket?
-    var startMillis = Date().millisecondsSince1970()
-    var plotTimer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupCharts()
         
-        self.plotTimer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
-        
-        socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
-        do {
-            try socket?.bind(toPort: 4201)
-            try socket?.beginReceiving()
-        } catch let err {
-            print("Error: ", err)
-        }
-    }
-    
-    @objc func timerAction() {
-        let diff = Date().millisecondsSince1970()! - self.startMillis!
-        actualLineChart.data!.addEntry(ChartDataEntry(x: Double(diff), y: lastTime), dataSetIndex: 0)
-        actualLineChart.notifyDataSetChanged();
+        #if targetEnvironment(simulator)
+            socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
+            do {
+                try socket?.bind(toPort: 4201)
+                try socket?.beginReceiving()
+            } catch let err {
+                print("Error: ", err)
+            }
+        #endif
     }
     
     func setupCharts() {
         // Setup the line chart view
         self.actualLineChart.delegate = self
-        let set: LineChartDataSet = LineChartDataSet(entries: [ChartDataEntry](), label: "RSSI")
+        let set: LineChartDataSet = LineChartDataSet(entries: [ChartDataEntry](), label: "Encoder Position")
         set.drawCirclesEnabled = false
         self.actualLineChart.data = LineChartData(dataSets: [set])
+        self.actualLineChart.updateTimeInterval = 0.025
+        self.actualLineChart.threshold = 10000
+        self.actualLineChart.start()
+        self.actualLineChart.extraLeftOffset = 0.0
     }
     
     func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
         let str = String(decoding: data, as: UTF8.self)
         let val = str.split(separator: "\n")[0]
         print(val)
-        addDataPoint(value: Double(val)!, datasetIndex: 0)
-    }
-    
-    var lastTime = 0.0
-    
-    func addDataPoint(value: Double, datasetIndex: Int) {
-        lastTime = value
-        if self.actualLineChart.data != nil {
-            let diff = Date().millisecondsSince1970()! - self.startMillis!
-            
-            self.actualLineChart.data!.addEntry(ChartDataEntry(x: Double(diff), y: value), dataSetIndex: datasetIndex)
-            
-            actualLineChart.notifyDataSetChanged();
-            
-//            self.actualLineChart.xAxis.axisMinimum = Double(diff-DA_BIG_THRESHOLD)
-            
-//            if Int(floor(Double(arc4random()) / Double(UINT32_MAX) * 10)) == 0 {
-//                if diff > DA_BIG_THRESHOLD {
-//                    //                    print("💨 spring cleaning\(diff - DA_BIG_THRESHOLD)")
-//                    for i in 0..<self.actualLineChart.data!.dataSets.count {
-//                        removeOldDataFor(datasetIndex: i)
-//                    }
-//                    actualLineChart.notifyDataSetChanged();
-//                }
-//            }
-        }
+        self.actualLineChart.addData(Double(val)!)
     }
 }
 
--- WifiDistanceMeasureApp/Views/UpdatableLineChartView.swift
+++ WifiDistanceMeasureApp/Views/UpdatableLineChartView.swift
@@ -9,6 +9,57 @@
 import Foundation
 import Charts
 
+// @See https://stackoverflow.com/a/49571718
+extension Date {
+    
+    func millisecondsSince1970() -> Int64! {
+        return Int64(self.timeIntervalSince1970 * 1000)
+    }
+    
+    init(millis: Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(millis / 1000))
+        self.addTimeInterval(TimeInterval(Double(millis % 1000) / 1000 ))
+    }
+    
+}
+
 class UpdatableLineChartView: LineChartView {
-    var threshold = 0
+    var timer: Timer?
+    var updateTimeInterval = 1.0
+    var threshold = 1000.0
+    var startMillis = Date().millisecondsSince1970()
+    var lastTime = 0.0
+    
+    func start() {
+        self.timer = Timer.scheduledTimer(timeInterval: self.updateTimeInterval, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerAction() {
+        #if targetEnvironment(simulator)
+            lastTime += ceil(Double(arc4random()) / Double(UINT32_MAX) * 10) - 5
+        #endif
+        self.addData(lastTime)
+    }
+    
+    func addData(_ x: Double) {
+        let diff = Double(Date().millisecondsSince1970()! - self.startMillis!)
+        self.data!.addEntry(ChartDataEntry(x: Double(diff), y: x), dataSetIndex: 0)
+        
+        self.xAxis.axisMinimum = diff - self.threshold
+        
+
+        if diff > self.threshold {
+            for i in 0..<self.data!.dataSets.count {
+                removeOldDataFor(datasetIndex: i)
+            }
+        }
+        self.notifyDataSetChanged()
+    }
+    
+    func removeOldDataFor(datasetIndex: Int) {
+        let dset = self.data!.getDataSetByIndex(datasetIndex)!
+        if let e = dset.entryForIndex(0), e.x < (Double(Date().millisecondsSince1970()) - self.threshold) {
+            _ = dset.removeEntry(dset.entryForIndex(0)!)
+        }
+    }
 }
